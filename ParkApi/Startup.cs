e7e18769a299973b80c3f743324a3ca636616db6
Nathan Conn <nconn34@gmail.com>
2022-04-23 16:50:58 -0700

    Added new code to Configure and ConfigureServices to implement Swagger

--- ParkApi/Startup.cs
+++ ParkApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ParkApi.Models;
 
 namespace StateAndNationalParks
@@ -24,17 +25,39 @@ namespace StateAndNationalParks
             services.AddDbContext<ParkApiContext>(opt =>
                 opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ParksAPI",
+                    Description = "API for parks"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParksAPI V");
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+            app.UseDeveloperExceptionPage();
             }
 
             // app.UseHttpsRedirection();
+            
+
+            
 
             app.UseRouting();
 
@@ -42,7 +65,7 @@ namespace StateAndNationalParks
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+            endpoints.MapControllers();
             });
         }
     }
