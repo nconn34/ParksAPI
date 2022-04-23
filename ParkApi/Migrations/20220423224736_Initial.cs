0a0840b48634802121f9ba88757aa49d409f16d5
Nathan Conn <nconn34@gmail.com>
2022-04-23 15:56:19 -0700

    Updated migrations to include full seed data

---
+++ ParkApi/Migrations/20220423224736_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CretaceousPark.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Parks",
+                columns: table => new
+                {
+                    ParkId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "varchar(20) CHARACTER SET utf8mb4", maxLength: 20, nullable: false),
+                    Type = table.Column<string>(type: "varchar(40) CHARACTER SET utf8mb4", maxLength: 40, nullable: false),
+                    Location = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true),
+                    YearFounded = table.Column<int>(type: "int", nullable: false),
+                    Activities = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parks", x => x.ParkId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Activities", "Location", "Name", "Type", "YearFounded" },
+                values: new object[] { 1, "Hiking", "Colorado", "Rockymountain", "NP", 1924 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Parks");
+        }
+    }
+}
---
+++ ParkApi/Migrations/20220423225318_SeedData1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CretaceousPark.Migrations
+{
+    public partial class SeedData1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "Activities", "Location", "Name", "Type", "YearFounded" },
+                values: new object[,]
+                {
+                    { 2, "Hiking", "Washington", "Mt.rainier", "NP", 1915 },
+                    { 3, "Surfing", "Washington", "Capedisappointment", "SP", 1936 },
+                    { 4, "Snowboarding", "Washington", "Mtbaker", "SP", 1946 },
+                    { 5, "Kayaking", "Wisconsin", "Wyalusing", "SP", 1926 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 5);
+        }
+    }
+}
